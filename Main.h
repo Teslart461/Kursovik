e946c84981af72f8186407f147e4ddb41b3ee51b
Teslart461 <apanteleev1001@gmail.com>
2024-12-21 20:09:41 +0700

    Create Main.h

---
+++ Main.h
@@ -0,0 +1,66 @@
+#pragma once
+
+namespace Kursovaya2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для Main
+	/// </summary>
+	public ref class Main : public System::Windows::Forms::Form
+	{
+	public:
+		Main(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~Main()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Main
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(839, 752);
+			this->Name = L"Main";
+			this->Text = L"Main";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+
+	};
+}
