dfef40e2b2696b4f152629eb708487e6a411bf12
Teslart461 <apanteleev1001@gmail.com>
2025-01-12 23:09:05 +0700

    Create and Update files
    Сделана проверка на существование файлов и проверка на дату. Diary.txt это не касается, но у него есть добавление даты, так как он используется для отчётов

--- Main.h
+++ Main.h
@@ -46,7 +46,7 @@ namespace Kursovaya2 {
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -69,9 +69,10 @@ namespace Kursovaya2 {
 			this->Label_welcome->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->Label_welcome->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->Label_welcome->Location = System::Drawing::Point(126, 66);
+			this->Label_welcome->Location = System::Drawing::Point(94, 54);
+			this->Label_welcome->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->Label_welcome->Name = L"Label_welcome";
-			this->Label_welcome->Size = System::Drawing::Size(335, 40);
+			this->Label_welcome->Size = System::Drawing::Size(278, 33);
 			this->Label_welcome->TabIndex = 0;
 			this->Label_welcome->Text = L"Добро пожаловать!";
 			// 
@@ -81,9 +82,10 @@ namespace Kursovaya2 {
 				static_cast<System::Int32>(static_cast<System::Byte>(51)));
 			this->button_profile->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button_profile->Location = System::Drawing::Point(218, 159);
+			this->button_profile->Location = System::Drawing::Point(164, 129);
+			this->button_profile->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button_profile->Name = L"button_profile";
-			this->button_profile->Size = System::Drawing::Size(163, 56);
+			this->button_profile->Size = System::Drawing::Size(122, 46);
 			this->button_profile->TabIndex = 1;
 			this->button_profile->Text = L"Профиль";
 			this->button_profile->UseVisualStyleBackColor = false;
@@ -94,9 +96,10 @@ namespace Kursovaya2 {
 				static_cast<System::Int32>(static_cast<System::Byte>(51)));
 			this->button_diary->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button_diary->Location = System::Drawing::Point(218, 261);
+			this->button_diary->Location = System::Drawing::Point(164, 212);
+			this->button_diary->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button_diary->Name = L"button_diary";
-			this->button_diary->Size = System::Drawing::Size(163, 56);
+			this->button_diary->Size = System::Drawing::Size(122, 46);
 			this->button_diary->TabIndex = 2;
 			this->button_diary->Text = L"Дневник";
 			this->button_diary->UseVisualStyleBackColor = false;
@@ -108,24 +111,27 @@ namespace Kursovaya2 {
 				static_cast<System::Int32>(static_cast<System::Byte>(36)), static_cast<System::Int32>(static_cast<System::Byte>(51)));
 			this->button_reports_of_nutrition->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold,
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(204)));
-			this->button_reports_of_nutrition->Location = System::Drawing::Point(218, 358);
+			this->button_reports_of_nutrition->Location = System::Drawing::Point(164, 291);
+			this->button_reports_of_nutrition->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button_reports_of_nutrition->Name = L"button_reports_of_nutrition";
-			this->button_reports_of_nutrition->Size = System::Drawing::Size(163, 56);
+			this->button_reports_of_nutrition->Size = System::Drawing::Size(122, 46);
 			this->button_reports_of_nutrition->TabIndex = 3;
 			this->button_reports_of_nutrition->Text = L"Отчёты";
 			this->button_reports_of_nutrition->UseVisualStyleBackColor = false;
 			// 
 			// Main
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::Black;
-			this->ClientSize = System::Drawing::Size(592, 508);
+			this->ClientSize = System::Drawing::Size(444, 413);
 			this->Controls->Add(this->button_reports_of_nutrition);
 			this->Controls->Add(this->button_diary);
 			this->Controls->Add(this->button_profile);
 			this->Controls->Add(this->Label_welcome);
 			this->ForeColor = System::Drawing::Color::DeepSkyBlue;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"Main";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"NutriTrack";
@@ -135,14 +141,68 @@ namespace Kursovaya2 {
 
 		}
 #pragma endregion
-	private: System::Void Main_Load(System::Object^ sender, System::EventArgs^ e) {
+	String^ getCurrentDate() {
+		DateTime now = DateTime::Now;
+		return now.ToString("dd-MM-yyyy");
+	}
+
+	void updateDiaryFile(String^ filename) {
+		String^ today = getCurrentDate();
+		if (!File::Exists(filename)) {
+			StreamWriter^ file = gcnew StreamWriter(filename);
+			file->WriteLine(today + ";");
+			file->Close();
+		}
+		else {
+			StreamReader^ file = gcnew StreamReader(filename);
+			String^ lastLine;
+			String^ line;
+			while ((line = file->ReadLine()) != nullptr) {
+				lastLine = line;
+			}
+			file->Close();
+
+			if (!lastLine->Contains(today)) {
+				StreamWriter^ file = gcnew StreamWriter(filename, true);
+				file->WriteLine(today + ";");
+				file->Close();
+			}
+		}
+	}
+
+	void createOrUpdateFile(String^ filename) {
+		String^ today = getCurrentDate();
+		if (!File::Exists(filename)) {
+			StreamWriter^ file = gcnew StreamWriter(filename);
+			file->WriteLine(today + ";");
+			file->Close();
+		}
+		else {
+			StreamReader^ file = gcnew StreamReader(filename);
+			String^ firstLine = file->ReadLine();
+			file->Close();
+
+			if (!firstLine->Contains(today)) {
+				StreamWriter^ file = gcnew StreamWriter(filename, false);
+				file->WriteLine(today + ";");
+				file->Close();
+			}
+		}
+	}
+
+	private: System::Void Main_Load(System::Object^ sender, System::EventArgs^ e) { // Сделана проверка на существование файлов и проверка на дату. Diary.txt это не касается, но у него есть добавление даты, так как он используется для отчётов
+		updateDiaryFile("diary.txt");
+		createOrUpdateFile("breakfast.txt");
+		createOrUpdateFile("lunch.txt");
+		createOrUpdateFile("dinner.txt");
+		createOrUpdateFile("snack.txt");
 	}
 
 	private: System::Void button_diary_Click(System::Object^ sender, System::EventArgs^ e) {
-		Diary^ p = gcnew Diary();
-		p->Show();
-		p->Owner = this;
+		Diary^ DiaryShow = gcnew Diary();
+		DiaryShow->Show();
+		DiaryShow->Owner = this;
 		this->Hide();
 	}
-};
+	};
 }
